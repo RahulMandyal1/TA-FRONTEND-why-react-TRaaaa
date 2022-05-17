c5f769fccf633a24742d4664a895d4329bfe7cf8
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-05-18 01:26:31 +0530

    Block: Assignment: Articles App in react js

---
+++ block-BRaaao/articlesapp/src/components/footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+    return (
+      <footer className="footer-container">
+        <p className="text-center"> all rights are reserved</p>
+      </footer>
+    )
+  }
+
+  export default Footer;
