1014007c58ee51a7699fee65fcf26c30f064837d
RahulMandyal1 <93306066+RahulMandyal1@users.noreply.github.com>
2022-05-19 14:27:28 +0530

    made hydro using react js

---
+++ block-BRaaav/hyrdrouserinterface/src/components/App.js
@@ -0,0 +1,21 @@
+import Navigationbar from "./header";
+import Herosection from "./herosection";
+import Aboutsection from "./aboutsection";
+import Blogsection from "./blogsection";
+import Worksection from "./worksection";
+import Contactsection from "./contactsection";
+import Footer from "./footer"
+
+function App(){
+    return <>
+    <Navigationbar/>
+    <Herosection/>
+    <Aboutsection/>
+    <Blogsection/>
+    <Worksection/>
+    <Contactsection/>
+    <Footer/>
+    </>
+}
+
+export default App;
